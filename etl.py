72a028475d791683ac093293f68ec2fbadddcb21
Koji-Sunioj <koji.gabriel218@gmail.com>
2026-04-03 23:27:07 +0300

    yalla

--- etl.py
+++ etl.py
@@ -13,6 +13,7 @@ args = getResolvedOptions(sys.argv, ["db_analyst", "etl_bucket"])
 
 db_params = args["db_analyst"]
 print(db_params)
+print(args)
 encoded = db_params.encode("utf-8")
 db_name, user, password, port = base64.b64decode(encoded).decode("utf-8").split(",")
 
